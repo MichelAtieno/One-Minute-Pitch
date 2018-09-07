803f2ea39a75d659a4eb42d30e9dbbcde55af4c3
Michel Atieno <mishqamish@gmail.com>
2018-09-07 18:04:07 +0300

    Added class Category to sort pitches into categories

--- app/models.py
+++ app/models.py
@@ -7,8 +7,23 @@ class User(db.Model):
     username = db.Column(db.String(255))
     email = db.Column(db.String(255))
 
+    pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+
     def __repr__(self):
         return f'User {self.username}'
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = db.Column(db.Integer,primary_key = True)
+    category_name = db.Column(db.String(255), index= True)
+
+    pitches = db.relationship('Pitch', backref='category', lazy='dynamic')
+
+    @classmethod
+    def get_pitch_categories(cls):
+        categories = Category.query.all()
+        return categories
 
 
 class Pitch(db.Model):
@@ -17,25 +32,24 @@ class Pitch(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     pitch_description = db.Column(db.String())
     pitch_category = db.Column(db.String(255))
+
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    category_id = db.Column(db.Integer,db.ForeignKey('categories.id'))
+
 
     @classmethod
     def save_pitch(self):
-        Pitch.all_pitches.append(self)
-
-    @classmethod
-    def clear_pitch(cls):
-        Pitch.all_pitches.clear()
+       db.session.add(self)
+       db.session.commit()
 
     @classmethod
     def get_pitch(cls,id):
+        pitches = Pitch.query.filter_by(category_id = id).all()
+        return pitches
+        
 
-        response = []
-
-        for pitch in cls.all_pitches:
-            if pitch.user_id == id:
-                response.append(pitch)
-
-        return response
+    #@classmethod
+    #def get_pitch_category:
+        
         
 
