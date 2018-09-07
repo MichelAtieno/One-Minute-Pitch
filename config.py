c1c50c31ebe3d8ad03cbbd5fe9c9eabf1058e071
Michel Atieno <mishqamish@gmail.com>
2018-09-07 14:54:58 +0300

    Initialized sqlalchemy in __init__

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michel:Mitch@localhost/pitch'
+
 
 
 
