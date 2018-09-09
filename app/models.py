c8cb57c1f4ab523f16c9ffaf42c89eabaf936d16
Michel Atieno <mishqamish@gmail.com>
2018-09-09 21:31:23 +0300

    Created class comment and identified relationships

--- app/models.py
+++ app/models.py
@@ -1,7 +1,12 @@
-from . import db
+from . import db, login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
@@ -10,6 +15,7 @@ class User(db.Model):
     pass_secure = db.Column(db.String(255))
 
     pitches = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comments = db.relationship('Comment', backref = 'user', lazy = "dynamic")
 
     @property
     def password(self):
@@ -49,6 +55,7 @@ class Pitch(db.Model):
 
     user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
     category_id = db.Column(db.Integer,db.ForeignKey('categories.id'))
+    comments = db.relationship('Comment', backref = 'pitch', lazy = "dynamic")
 
 
     @classmethod
@@ -60,6 +67,27 @@ class Pitch(db.Model):
     def get_pitch(cls,id):
         pitches = Pitch.query.filter_by(category_id = id).all()
         return pitches
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer, primary_key = True)
+    post_comment = db.Column(db.String(255), index = True)
+    #time = db.Column(db.DateTime, default = datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls, id):
+        comments = Comment.query.filter_by(pitch_id=id).all()
+        return comments
+
+
         
         
         
