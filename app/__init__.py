4c96b141e3e7dd45f87b33e9b5913f4e627dd139
Michel Atieno <mishqamish@gmail.com>
2018-09-10 00:08:15 +0300

    User can add image to profile

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -18,6 +20,9 @@ def create_app(config_name):
     #Setting up configuration
     app.config.from_object(config_options[config_name])
     #config_options[config_name].init_app(app)
+
+    # configure UploadSet
+    configure_uploads(app,photos)
 
     #Initializing flask extensions
     bootstrap.init_app(app)
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michel:Mitch@localhost/pitch'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 
 
