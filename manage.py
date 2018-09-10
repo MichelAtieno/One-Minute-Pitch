ce66c52e51db6475fb9a6211c69b629ce8d7d1bb
Michel Atieno <mishqamish@gmail.com>
2018-09-10 14:27:10 +0300

    edited config.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app.models import User, Pitch
 
 
 #Creating app instance
-app = create_app('test')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
