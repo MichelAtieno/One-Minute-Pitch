6af5c176f1e4672c710331517f0461d0ff26ec91
Michel Atieno <mishqamish@gmail.com>
2018-09-12 12:18:57 +0300

    Added Database info to DevConfig

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michel:Mitch@localhost/pitch_test'
 
 class DevConfig(Config):
-    
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
     DEBUG = True
 
 config_options = {
