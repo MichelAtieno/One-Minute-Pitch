716c6a5da41eecaf84d41af40f5becf342967654
Michel Atieno <mishqamish@gmail.com>
2018-09-12 12:41:25 +0300

    Edited login in auth

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,7 +13,7 @@ def login():
         user = User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user, login_form.remember.data)
-            return redirect(request.args.get('next') or url_for('main.index'))
+            return redirect(request.args.get('next') or url_for('main.pitch'))
 
         flash('Invalid username or password')
 
@@ -24,14 +24,13 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!','success')
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
 
         mail_message("Welcome to pitch haven","email/welcome_user",user.email,user=user)
         return redirect(url_for('auth.login'))
-        title = "New Account"
+
     return render_template('auth/register.html',registration_form = form)
 
 @auth.route('/logout')
