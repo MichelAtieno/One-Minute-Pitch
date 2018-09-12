752190adffdeb3b9333ffea4ad79fd89e135ded4
Michel Atieno <mishqamish@gmail.com>
2018-09-12 09:11:55 +0300

    Changed to production

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://michel:Mitch@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
     #  email configurations
